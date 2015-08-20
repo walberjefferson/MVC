6b1f69210d3c96eba2f56c2a222923522140ef01
Walber Jefferson <walberjefferson@hotmail.com>
2015-08-20 14:51:51 -0300

    Update View
    Render and Content

--- vendor/SON/Controller/Action.php
+++ vendor/SON/Controller/Action.php
@@ -7,16 +7,27 @@ namespace SON\Controller;
 class Action
 {
     protected $view;
+    protected $action;
 
     public function __construct()
     {
         $this->view = new \stdClass;
     }
 
-    public function render($action)
+    public function render($action, $layout=true)
+    {
+        $this->action = $action;
+        if($layout == true && file_exists('../App/Views/layout.phtml')){
+            include_once '../App/Views/layout.phtml';
+        }else {
+            $this->content();
+        }
+    }
+
+    public function content()
     {
         $atual = get_class($this);
-        $singleClassName = strtolower(str_replace("App\\Controllers\\","",$atual));
-        include_once '../App/Views/'.$singleClassName.'/'.$action.'.phtml';
+        $singleClassName = strtolower(str_replace("App\\Controllers\\", "", $atual));
+        include_once '../App/Views/' . $singleClassName . '/' . $this->action . '.phtml';
     }
 }
